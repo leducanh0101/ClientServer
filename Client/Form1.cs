c656dd77da3e4556c559a2a2843eac58da721547
Anh <biebo0101@gmail.com>
2018-03-13 22:20:53 +0700

    80%

---
+++ Client/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        byte[] data;
+        IPEndPoint ipServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1") , 1234);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(ipServer);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox2.Text;
+            data = new Byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            textBox2.Text = "";
+            listBox1.Items.Add(text);
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            text = Encoding.ASCII.GetString(data);
+            listBox1.Items.Add(text);
+        }
+
+    }
+}
---
+++ ServerChat/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerChat
+{
+    public partial class Form1 : Form
+    {
+        Socket server, client;
+        byte[] data;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipServer);
+            server.Listen(5);
+            client = server.Accept();
+            data = new byte[1024];
+            client.Receive(data);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data));
+        
+            
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = textBox3.Text;
+            listBox1.Items.Add(text);
+            textBox3.Text = "";
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data));
+        }
+    }
+}
